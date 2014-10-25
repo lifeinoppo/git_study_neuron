15545b18affe1bf34611309d4d6e135f113d48a4
jxc <jxcjxcjzx@sina.com>
2014-10-25 23:21:04 +0800

    backpropogation commit

---
+++ BackProp_C/BackPropogation.cpp
@@ -0,0 +1,15 @@
+// this is the c implementation of back propogation neuron network  
+
+
+// tiny functions
+RandNumberType rand(RandNumberType a, RandNumberType b)
+{
+
+//  a.minus(b) means a-b
+return (a.minus(b).multi(Rand.random()).plus(a));
+
+}
+
+// make matrix
+MatrixType 
+
